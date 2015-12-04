fddf2438f9d39609e726d4d5b089a72976c4f438
TakashiP <masai.takashi@gmail.com>
2015-12-04 10:02:01 +0000

    revised

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,11 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:show, :edit, :update]
+before_action :set_user, only: [:edit, :update]
   def new
     @user = User.new
   end
 
   def show
-    if @user != current_user
-      redirect_to root_url
-    else
     @user = User.find(params[:id])
-    end
   end
   
   def create
@@ -29,7 +25,7 @@ before_action :set_user, only: [:show, :edit, :update]
   end
   
   def update
-    if @user = current_user
+    if @user == current_user
       if @user.update(user_params)
         # 保存に成功した場合はプロフィールページへリダイレクト
         redirect_to @user , notice: 'プロフィールを編集しました'
