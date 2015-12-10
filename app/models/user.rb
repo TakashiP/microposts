af2adda593ba6fb10324930db6cb0cf346392ace
TakashiP <masai.takashi@gmail.com>
2015-12-10 07:17:14 +0000

    revised, hopefully this is the final revision

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ before_save { self.email = email.downcase }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
     has_secure_password
+    validates :personal_info, presence: true, length: { maximum: 200 }
+    validates :address, presence: true, length: { maximum: 200 }
 end
